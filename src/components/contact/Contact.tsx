a1fceedd14e8e316cae9277a35e4b454c97cdb4e
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-07-17 19:50:08 -0700

    mail service and contact links

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { toast } from 'react-hot-toast';
 
 type Props = {};
 
@@ -13,20 +14,22 @@ const Contact = (props: any) => {
 
   const [{ name, email, message }, setState] = useState(initialState);
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
   const clearState = () => setState({ ...initialState });
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(name, email, message);
+    console.log('sending form target', e.target);
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, e.target as HTMLFormElement, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
       .then(
         (result) => {
           console.log(result.text);
+          toast.success('Thank you for your email!');
           clearState();
         },
         (error) => {
@@ -44,7 +47,7 @@ const Contact = (props: any) => {
               <div className="section-title">
                 <h2>Get In Touch</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
+                  Please fill out the form below to send me an email and I will
                   get back to you as soon as possible.
                 </p>
               </div>
@@ -103,10 +106,10 @@ const Contact = (props: any) => {
               <h3>Contact Info</h3>
               <p>
                 <span>
-                  <i className="fa fa-map-marker"></i> Address
+                  <i className="fa fa-map-marker"></i> Location
                 </span>
                 {/* {props.data ? props.data.address : "loading"} */}
-                Address
+                {"Vancouver, BC"}
               </p>
             </div>
             <div className="contact-item">
@@ -115,7 +118,7 @@ const Contact = (props: any) => {
                   <i className="fa fa-phone"></i> Phone
                 </span>{" "}
                 {/* {props.data ? props.data.phone : "loading"} */}
-                Phone number
+                Get in touch
               </p>
             </div>
             <div className="contact-item">
@@ -124,7 +127,7 @@ const Contact = (props: any) => {
                   <i className="fa fa-envelope-o"></i> Email
                 </span>{" "}
                 {/* {props.data ? props.data.email : "loading"} */}
-                Email address
+                Get in touch
               </p>
             </div>
           </div>
@@ -134,22 +137,17 @@ const Contact = (props: any) => {
                 <ul>
                   <li>
                     <a
-                      href={props.data ? props.data.facebook : "/"}
+                      href={"https://github.com/m3c-ode"}
                     >
                       {/* Facebook address */}
-                      <i className="fa fa-facebook"></i>
+                      <i className="fa fa-github"></i>
                     </a>
                   </li>
                   <li>
                     <a
-                      href={props.data ? props.data.twitter : "/"}
+                      href={"https://www.linkedin.com/in/maxime-marechal-mccoy/"}
                     >
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
+                      <i className="fa fa-linkedin"></i>
                     </a>
                   </li>
                 </ul>
