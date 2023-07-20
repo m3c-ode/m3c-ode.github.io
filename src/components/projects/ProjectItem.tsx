d7c1616fa4a2cf5022900c59efe88118e6403483
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-07-19 22:18:06 -0700

    added gallery for projects and content

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -1,66 +1,109 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import ReactImageGallery from 'react-image-gallery';
+import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
+import './style.css';
 
 type Props = {
   title?: string,
+  description?: string,
+  features?: string[],
+  prodLink?: string,
+  prodNote?: string,
+  githubLink?: string,
   imgSource?: string,
+  imgObject?: ReactImageGalleryItem[],
   imgPos: {
     right?: boolean,
     left?: boolean,
   };
 };
 
-const ProjectItem = (props: Props) => {
+const ProjectItem = ({
+  title,
+  description,
+  features,
+  prodLink,
+  prodNote,
+  githubLink,
+  imgSource,
+  imgObject,
+  imgPos }: Props) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const imageGalleryRef = useRef<ImageGallery | null>(null);
+
+  const openGallery = () => {
+    setIsOpen(true);
+    imageGalleryRef.current?.fullScreen();
+  };
+
+  const closeGallery = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <div className="project-item row">
         {/* <p>Item</p> */}
-        {props.imgPos.left &&
+        {imgPos.left &&
           <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            {/* <img onClick={openGallery} src={imgSource ? imgSource : "img/about.jpg"} className="img-responsive" alt="" />{" "} */}
+
+            {/* <div className="img-responsive img-container"> */}
+            <ImageGallery
+              ref={imageGalleryRef}
+              items={imgObject!}
+              showFullscreenButton={true}
+              showBullets={true}
+              showThumbnails={false}
+              showPlayButton={false}
+            />
+            {/* </div> */}
           </div>
         }
         <div className="col-xs-12 col-md-6">
           <div className="project-text">
             <div className="project-title">
-              <h3>{props.title}</h3>
+              <h3><a href={githubLink}>{title}</a></h3>
             </div>
-            {/* <p>{props.data ? props.data.paragraph : "loading..."}</p> */}
-            <p>Project presentation</p>
+            <p>{description ? description : "loading..."}</p>
+            <p>Link to deployed application: <a href={prodLink}>{prodLink}</a> {prodNote}</p>
             <h3>Features</h3>
             <div className="list-style">
               <div className="col-lg-6 col-sm-6 col-xs-12">
                 <ul>
-                  {/* {props.data
-                        ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                        : "loading"} */}
-                  <li>This is a test</li>
-                  <li>This is a 2nd test</li>
+                  {features
+                    ? features.map((d, i) => (
+                      i % 2 === 0 ? <li key={`${d}-${i}`}>{d}</li> : null
+                    ))
+                    : "loading"}
                 </ul>
               </div>
               <div className="col-lg-6 col-sm-6 col-xs-12">
                 <ul>
-                  {/* {props.data
-                        ? props.data.Why2.map((d, i) => (
+                  {features
+                    ? features.map((d, i) => (
+                      i % 2 === 1 ? <li key={`${d}-${i}`}>{d}</li> : null
+                    ))
+                    : "loading"}
+                  {/* {data
+                        ? data.Why2.map((d, i) => (
                           <li key={`${d}-${i}`}> {d}</li>
                         ))
                         : "loading"} */}
-                  <li>Some other bullet</li>
-                  <li>Some other bullet</li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-        {props.imgPos.right &&
+        {imgPos.right &&
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/portfolio/m3b-bms-1.png" className="img-responsive" alt="" />{" "}
           </div>
         }
       </div>
+
     </>
   );
 };
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
 import ProjectItem from './ProjectItem';
+import { ReactImageGalleryItem } from 'react-image-gallery';
+import content from './content.json';
 
 type Props = {};
+
+const bmsImages: ReactImageGalleryItem[] = [
+  {
+    original: 'img/portfolio/m3b-bms-1.png',
+    description: 'Product table example',
+  },
+  {
+    original: 'img/portfolio/m3b-bms-2.png',
+    description: 'New product creation - step form',
+
+  },
+  {
+    original: 'img/portfolio/m3b-bms-3.png',
+    description: 'Delivery service with Google API implementation - Places, Directions',
+  },
+  {
+    original: 'img/portfolio/m3b-bms-4.png',
+    description: 'Client base management with delivery option',
+
+  },
+];
+
+const elmImages: ReactImageGalleryItem[] = [
+  {
+    original: 'img/portfolio/m3b-bms-1.png',
+    description: 'Product table example',
+  },
+  {
+    original: 'img/portfolio/m3b-bms-2.png',
+    description: 'New product creation - step form',
+
+  },
+  {
+    original: 'img/portfolio/m3b-bms-3.png',
+    description: 'Delivery service with Google API implementation - Places, Directions',
+  },
+  {
+    original: 'img/portfolio/m3b-bms-4.png',
+    description: 'Client base management with delivery option',
+
+  },
+];
 
 const Projects = (props: Props) => {
   return (
@@ -11,8 +54,28 @@ const Projects = (props: Props) => {
           <div className="section-title">
             <h2 className="text-center">Projects</h2>
           </div>
-          <ProjectItem title='m3.beer Brewery Management Service' imgPos={{ left: true }} />
-          <ProjectItem title="Employee Management Application" imgPos={{ right: true }} />
+          <ProjectItem
+            // title='m3.beer Brewery Management Service'
+            imgPos={{ left: true }}
+            // imgSource='img/portfolio/m3b-bms-2.png'
+            imgObject={bmsImages}
+            title={content['m3b-bms'].title}
+            description={content['m3b-bms'].description}
+            features={content['m3b-bms'].features}
+            prodLink={content['m3b-bms'].prodLink}
+            githubLink={content['m3b-bms'].githubLink}
+          />
+          <ProjectItem
+            // title="Employee Management Application"
+            imgPos={{ right: true }}
+            imgObject={bmsImages}
+            title={content['ems'].title}
+            description={content['ems'].description}
+            features={content['ems'].features}
+            prodLink={content['ems'].prodLink}
+            githubLink={content['ems'].githubLink}
+            prodNote={"(currently offline due to change in plan subscription)"}
+          />
         </div>
       </div>
     </div>
