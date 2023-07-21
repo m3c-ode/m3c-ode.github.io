a81b47b74faab98ad04a58a159367014651db725
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-07-21 09:55:40 -0700

    cleanup 2

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,45 +5,6 @@ type Props = {};
 
 const Navigation = (props: Props) => {
   return (
-    // <nav className="navbar navbar-expand-md bg-light">
-    //   <div className="container-fluid">
-    //     <a className="navbar-brand" href="#page-top">m3.code</a>
-    //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-    //         <li className="nav-item">
-    //           <a className="nav-link active" aria-current="page" href="#">Home</a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link" href="#">Link</a>
-    //         </li>
-    //         <li className="nav-item dropdown">
-    //           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    //             Dropdown
-    //           </a>
-    //           <ul className="dropdown-menu">
-    //             <li><a className="dropdown-item" href="#">Action</a></li>
-    //             <li><a className="dropdown-item" href="#">Another action</a></li>
-    //             <li><hr className="dropdown-divider" /></li>
-    //             <li><a className="dropdown-item" href="#">Something else here</a></li>
-    //           </ul>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link disabled">Disabled</a>
-    //         </li>
-    //       </ul>
-    //       {/* <form className="d-flex" role="search">
-    //         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-    //         <button className="btn btn-outline-success" type="submit">Search</button>
-    //       </form> */}
-    //     </div>
-    //   </div>
-    // </nav>
-
-
-
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container-fluid">
         <div className="navbar-header">
@@ -78,26 +39,6 @@ const Navigation = (props: Props) => {
                 Projects
               </a>
             </li>
-            {/* <li>
-              <a href="#services" className="page-scroll">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#portfolio" className="page-scroll">
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href="#testimonials" className="page-scroll">
-                Testimonials
-              </a>
-            </li>
-            <li>
-              <a href="#team" className="page-scroll">
-                Team
-              </a>
-            </li> */}
             <li>
               <a href="#contact" className="page-scroll">
                 Contact
